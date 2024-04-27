c84da251fb6a158a6dd58b28da4e5b07c509494c
Igwe Ugochukwu Michael Sylvester <elinteerie@gmail.com>
2024-04-27 22:37:44 +0100

    setting up project environe

---
+++ src/authorsapi/settings/local.py
@@ -0,0 +1,9 @@
+
+
+
+SECRET_KEY = "django-insecure-c_qn5nor72qrf*&n5g087jyg#o4g@*nxh1*&#f+*g&3%f4k0nq"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
